a1bcdf42006158754f78cb79171e68e3b548e908
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-19 22:17:33 +0530

    🔄 implement browser router in App component...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Event from "./pages/Event";
+import EventDetail from "./pages/EventDetail";
+import NewEvent from "./pages/NewEvent";
+import EditEvent from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Home />,
+   },
+   {
+      path: "/events",
+      element: <Event />,
+   },
+   {
+      path: "/events/:id",
+      element: <EventDetail />,
+   },
+   {
+      path: "/events/new",
+      element: <NewEvent />,
+   },
+   {
+      path: "/events/:id/edit",
+      element: <EditEvent />,
+   },
+]);
+
 function App() {
-   return (
-      <>
-         <h1>Hello World</h1>
-      </>
-   );
+   return <RouterProvider router={router} />;
 }
 
 export default App;
